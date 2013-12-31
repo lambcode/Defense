6fd299b0077dd29d68253f19f5979a6ccff58f78
Brian Lamb <leobl13@yahoo.com>
2013-12-31 16:32:05 -0700

    Add: Superclass Logic
    TopMenu inherits Logic which reduces the amount of methods in TopMenu.
    Can now be used to make logic loops for all game areas.

---
+++ Defense/src/com/solidapt/defense/Logic.java
@@ -0,0 +1,36 @@
+package com.solidapt.defense;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import com.solidapt.mainMenu.TopMenu;
+
+public abstract class Logic {
+
+	static Logic myLogic;
+
+	public static void logicLoop(double time) {
+		if (myLogic == null)
+			TopMenu.load();
+		
+		myLogic.doLogicLoop(time);
+	}
+
+	public static void renderLoop(GL10 gl) {
+		if (myLogic == null)
+			TopMenu.load();
+		
+		myLogic.doRenderLoop(gl);
+	}
+
+	public static void load() {
+		myLogic = new TopMenu();
+	}
+
+	public static void release() {
+		myLogic = null;
+	}
+	
+	public abstract void doLogicLoop(double time);
+	public abstract void doRenderLoop(GL10 gl);
+
+}
--- Defense/src/com/solidapt/mainMenu/TopMenu.java
+++ Defense/src/com/solidapt/mainMenu/TopMenu.java
@@ -4,52 +4,29 @@ import javax.microedition.khronos.opengles.GL10;
 
 import com.solidapt.citydefense.objects.GameObject;
 import com.solidapt.citydefense.objects.HostileMissile;
+import com.solidapt.defense.Logic;
 import com.solidapt.defense.SoundLoader;
 import com.solidapt.defense.Util;
 
-public class TopMenu {
+public class TopMenu extends Logic {
 	
-	static TopMenu topMenu;
 	GameObject background;
 	GameObject buttonMissile;
 	
 	public TopMenu() {
 		background = new MainMenuBackground();
 		buttonMissile = new ButtonMissile((int) (Util.getWidth()/2.5), Util.getHeight()+98, 48, 96, (int)(Util.getWidth() / 2.01), (int) (Util.getHeight()*.65), 400);
-		//new ButtonMissile(Util.getWidth()/2, Util.getHeight() + 65, 30, 60, Util.getWidth()/3, (int) (Util.getHeight()*.6), 100);
 	}
 	
-	public static void logicLoop(double time) {
-		if (topMenu == null)
-			TopMenu.load();
-		
-		topMenu.doLogicLoop(time);
-	}
-	
-	public static void renderLoop(GL10 gl) {
-		if (topMenu == null)
-			TopMenu.load();
-		
-		topMenu.doRenderLoop(gl);
-	}
-	
-	private void doRenderLoop(GL10 gl) {
+	public void doRenderLoop(GL10 gl) {
 		background.gameRenderLoop(gl);
 		buttonMissile.gameRenderLoop(gl);
 	}
 
-	private void doLogicLoop(double time) {
+	public void doLogicLoop(double time) {
 		SoundLoader.startMusic(SoundLoader.menuMusic);
 		background.gameLoopLogic(time);
 		buttonMissile.gameLoopLogic(time);
-	}
-
-	public static void load() {
-		TopMenu.topMenu = new TopMenu();
-	}
-	
-	public static void release() {
-		TopMenu.topMenu = null;
 	}
 
 }
