196fcb9da3518b29695928d7b7bc1976bc967eb0
Brian Lamb <leobl13@yahoo.com>
2014-01-05 10:53:10 -0700

    Add: InGame class for game logic and rendering

---
+++ Defense/src/inGame/InGame.java
@@ -0,0 +1,29 @@
+package inGame;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.view.MotionEvent;
+
+import com.solidapt.defense.Logic;
+
+public class InGame extends Logic {
+
+	@Override
+	public void doLogicLoop(double time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doRenderLoop(GL10 gl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doTouchEvent(MotionEvent e, float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
